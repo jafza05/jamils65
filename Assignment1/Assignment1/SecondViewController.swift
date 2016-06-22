a57cb06ad4bb9c0788c966e4c5999fc5f21b910b
jafza05 <jamilafza@gmail.com>
2016-06-22 18:41:24 -0400

    L2
    Lecture2 Test Stuff

---
+++ Assignment1/Assignment1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assignment1
+//
+//  Created by Jamil Afza on 6/22/16.
+//  Copyright © 2016 Jamil Afza. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

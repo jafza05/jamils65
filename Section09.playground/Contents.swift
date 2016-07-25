b67de4d6acddf8cdcf5ac6da2695f422fa2d5765
jafza05 <jamilafza@gmail.com>
2016-07-25 15:27:29 -0400

    Assignment bonus concepts added

--- Section09.playground/Contents.swift
+++ Section09.playground/Contents.swift
@@ -1,13 +1,8 @@
 //: Playground - noun: a place where people can play
 
+import UIKit
 
-/* Bonus Proposal
- 
- My interest is in the area of business intelligence and analytics. 
- 
- My bonus proposal probably won't sound too sophisticated but I would like to try to add some graphical analytics to the Statistics tab that chart the number of cells in each state over a rolling period of 5-10 iterations. And the percentage breakdown of how many cells of each state are currently on the grid. 
- 
- */
+var str = "Hello, playground"
 
 func isLeap(input: Int) ->  Bool {
     return (input % 4 == 0 && input % 100 != 0) || input % 400 == 0  ? true : false
@@ -35,10 +30,3 @@ julianDate(1901, month: 1, day: 1) - julianDate(1900, month: 1, day: 1)
 julianDate(2001, month: 1, day: 1) - julianDate(2000, month: 1, day: 1)
 
 
-
-//testing for my own purposes
-julianDate(1960, month:  2, day: 28)
-julianDate(1961, month:  2, day: 28)
-
-julianDate(1960, month:  3, day: 28)
-julianDate(1961, month:  3, day: 28)
